e9493edeec89102e90e04be688afcca84183a386
Matt Moore <matt@ekotechnology.net>
2014-05-13 21:39:53 -0600

    Removed PHP FPM

--- scripts/php.sh
+++ scripts/php.sh
@@ -20,7 +20,7 @@ fi
 sudo apt-get update
 
 # Install PHP
-sudo apt-get install -y php5-cli php5-fpm php5-mysql php5-pgsql php5-sqlite php5-curl php5-gd php5-gmp php5-mcrypt php5-xdebug php5-memcached php5-imagick php5-intl
+sudo apt-get install -y php5-cli libapache2-mod-php5 php5-mysql php5-pgsql php5-sqlite php5-curl php5-gd php5-gmp php5-mcrypt php5-xdebug php5-memcached php5-imagick php5-intl
 
 # xdebug Config
 cat > $(find /etc/php5 -name xdebug.ini) << EOF
@@ -38,18 +38,4 @@ xdebug.var_display_max_children = 256
 xdebug.var_display_max_data = 1024 
 EOF
 
-# PHP Error Reporting Config
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
-sed -i "s/html_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
-
-# PHP Date Timezone
-sed -i "s/;date.timezone =.*/date.timezone = ${2/\//\\/}/" /etc/php5/fpm/php.ini
-sed -i "s/;date.timezone =.*/date.timezone = ${2/\//\\/}/" /etc/php5/cli/php.ini
-
-# Make sure php5-fpm is running as a Unix socket on "distributed" version
-if [ $php_version == "distributed" ]; then
-    sed -i "s/listen = .*/listen = \/var\/run\/php5-fpm.sock/" /etc/php5/fpm/pool.d/www.conf
-fi
-
-sudo service php5-fpm restart
+sudo service apache2 restart

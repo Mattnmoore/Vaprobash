fd05b217f19187266f03d831c55132be063172e3
Matt Moore <matt@ekotechnology.net>
2014-05-13 21:41:04 -0600

    Remove PHP FPM

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -25,20 +25,4 @@ sudo mv vhost /usr/local/bin
 # Create a virtualhost to start, with SSL certificate
 sudo vhost -s $1.xip.io -d /vagrant -p /etc/ssl/xip.io -c xip.io
 
-# PHP Config for Apache
-cat > /etc/apache2/conf-available/php5-fpm.conf << EOF
-<IfModule mod_fastcgi.c>
-        AddHandler php5-fcgi .php
-        Action php5-fcgi /php5-fcgi
-        Alias /php5-fcgi /usr/lib/cgi-bin/php5-fcgi
-        FastCgiExternalServer /usr/lib/cgi-bin/php5-fcgi -socket /var/run/php5-fpm.sock -pass-header Authorization
-        <Directory /usr/lib/cgi-bin>
-                Options ExecCGI FollowSymLinks
-                SetHandler fastcgi-script
-                Require all granted
-        </Directory>
-</IfModule>
-EOF
-sudo a2enconf php5-fpm
-
 sudo service apache2 restart
